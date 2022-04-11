7e2ba2f287935615816f9313b749336c88c4124a
JonathanAssis <62781731+JonathanAssis@users.noreply.github.com>
2022-04-10 21:49:44 -0300

    Configurando o miragejs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,26 @@ import { Dashboard } from '../components/Dashboard'
 import { Header } from '../components/Header'
 import { TransactionsTable } from '../components/TransactionsTable'
 import { GlobalStyled } from '../styles/global'
+import { createServer } from 'miragejs'
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 const Home = () => {
   return (
